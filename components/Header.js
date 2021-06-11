808b1a6a3abfd3dc586c3888996055a863ffe8f4
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-06-11 18:49:08 +0530

    fetch working

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Image from 'next/image';
 import HeaderItem from './HeaderItem';
-import { HomeIcon } from '@heroicons/react/outline';
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon,
+} from '@heroicons/react/outline';
 
 export default function Header() {
     return (
-        <header className="">
-            <div>
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
+            <div className="mt-8 flex flex-grow justify-evenly max-w-2xl">
                 <HeaderItem title="HOME" Icon={HomeIcon} />
+                <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+                <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+                <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+                <HeaderItem title="SEARCH" Icon={SearchIcon} />
+                <HeaderItem title="ACCOUNT" Icon={UserIcon} />
             </div>
             <Image
                 className="object-contain"
