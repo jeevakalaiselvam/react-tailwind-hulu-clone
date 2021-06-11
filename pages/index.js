cd7b73ad88cc5978540b9bef28a119377b97fe18
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-06-11 19:07:40 +0530

    fetch response working

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,7 @@ import Results from '../components/Results';
 import requests from '../utils/requests';
 
 export default function Home({ results }) {
-    console.log(props);
-
+    console.log(results);
     return (
         <div>
             <Head>
@@ -29,7 +28,7 @@ export async function getServerSideProps(context) {
     const genre = context.query.genre;
 
     const request = await fetch(
-        `https://api.themoviedb.org/3/${
+        `https://api.themoviedb.org/3${
             requests[genre]?.url || requests.fetchTrending.url
         }`
     ).then((res) => res.json());
